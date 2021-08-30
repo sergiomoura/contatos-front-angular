fde2dac126fd9ee43b5bd0fa0bc6e5ed7029f00b
Sérgio Moura <smouracalmon@gmail.com>
2021-08-30 04:21:07 -0300

    Cirnado model Contato e fazendo ngFor para exibir contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome:"Contato 1",
+      email:"contato1@teste.com",
+      telefones:['1111111111']
+    },
+    {
+      nome:"Contato 2",
+      email:"contato2@teste.com",
+      telefones:['2222222222']
+    },
+    {
+      nome:"Contato 3",
+      email:"contato3@teste.com",
+      telefones:['3333333333']
+    },
+    {
+      nome:"Contato 4",
+      email:"contato4@teste.com",
+      telefones:['4444444444']
+    },
+    {
+      nome:"Contato 5",
+      email:"contato5@teste.com",
+      telefones:['5555555555']
+    },
+    {
+      nome:"Contato 6",
+      email:"contato6@teste.com",
+      telefones:['6666666666']
+    },
+    {
+      nome:"Contato 7",
+      email:"contato7@teste.com",
+      telefones:['7777777777']
+    },
+    {
+      nome:"Contato 8",
+      email:"contato8@teste.com",
+      telefones:['8888888888']
+    },
+    {
+      nome:"Contato 9",
+      email:"contato9@teste.com",
+      telefones:['999999999']
+    },
+    {
+      nome:"Contato 10",
+      email:"contato10@teste.com",
+      telefones:['1010101010']
+    },
+  ]
 
   constructor() { }
 

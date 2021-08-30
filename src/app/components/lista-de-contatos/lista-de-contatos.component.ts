52dac3adeb4389f9f6c1950b467a93e143b4612e
Sérgio Moura <smouracalmon@gmail.com>
2021-08-30 07:10:20 -0300

    Externalizando os contatos para o seviço ContatoService

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contato } from 'src/app/models/Contato';
+import { ContatoService } from 'src/app/services/contatos.service';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -8,60 +9,12 @@ import { Contato } from 'src/app/models/Contato';
 })
 export class ListaDeContatosComponent implements OnInit {
 
-  contatos:Contato[] = [
-    {
-      nome:"Contato 1",
-      email:"contato1@teste.com",
-      telefones:['1111111111']
-    },
-    {
-      nome:"Contato 2",
-      email:"contato2@teste.com",
-      telefones:['2222222222','484888844','455888977']
-    },
-    {
-      nome:"Contato 3",
-      email:"contato3@teste.com",
-      telefones:['3333333333']
-    },
-    {
-      nome:"Contato 4",
-      email:"contato4@teste.com",
-      telefones:['4444444444']
-    },
-    {
-      nome:"Contato 5",
-      email:"contato5@teste.com",
-      telefones:['5555555555']
-    },
-    {
-      nome:"Contato 6",
-      email:"contato6@teste.com",
-      telefones:['6666666666']
-    },
-    {
-      nome:"Contato 7",
-      email:"contato7@teste.com",
-      telefones:['7777777777']
-    },
-    {
-      nome:"Contato 8",
-      email:"contato8@teste.com",
-      telefones:['8888888888']
-    },
-    {
-      nome:"Contato 9",
-      email:"contato9@teste.com",
-      telefones:['999999999']
-    },
-    {
-      nome:"Contato 10",
-      email:"contato10@teste.com",
-      telefones:['1010101010']
-    },
-  ]
+  contatos:Contato[] = [];
+  private contatoService:ContatoService = new ContatoService();
 
-  constructor() { }
+  constructor() {
+    this.contatos = this.contatoService.getContatos();
+  } 
 
   ngOnInit(): void {
   }

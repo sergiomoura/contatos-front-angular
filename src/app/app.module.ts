fd5a59ccc5ef1eba16d1e0559646bd9a0cef624f
Sérgio Moura <smouracalmon@gmail.com>
2021-08-29 23:43:52 -0300

    Criado o componente ListaDeContatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ListaDeContatosComponent } from './components/lista-de-contatos/lista-de-contatos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ListaDeContatosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-contatos',
+  templateUrl: './lista-de-contatos.component.html',
+  styleUrls: ['./lista-de-contatos.component.css']
+})
+export class ListaDeContatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

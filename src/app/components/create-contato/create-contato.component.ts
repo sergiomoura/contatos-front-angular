f92e4683ecd84dbe8051217135f0f4b09aeb17f1
Sérgio Moura <smouracalmon@gmail.com>
2021-09-01 16:34:48 -0300

    Associando os campos do form de add contato ao atributo novoContato

--- src/app/components/create-contato/create-contato.component.ts
+++ src/app/components/create-contato/create-contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-create-contato',
@@ -8,12 +9,22 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CreateContatoComponent implements OnInit {
 
   @Output() onCancelarClick:EventEmitter<null> = new EventEmitter();
+  
+  novoContato:Contato = {
+    nome:"",
+    email:"",
+    telefones:[""]
+  }
 
   constructor() { }
 
   cancelar(){
     console.log("Pediu para candelar");
     this.onCancelarClick.emit()
+  }
+
+  track(index:number, value:string){
+    return index;
   }
 
   ngOnInit(): void {

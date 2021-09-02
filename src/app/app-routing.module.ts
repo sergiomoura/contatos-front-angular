218ee24a88ca3da05d71a7aa5e298e1a8cc3381c
Sérgio Moura <smouracalmon@gmail.com>
2021-09-02 10:02:08 -0300

    Configurando as rotas e o router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from "./components/sobre/sobre.component";
+import { ListaDeContatosComponent } from "./components/lista-de-contatos/lista-de-contatos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaDeContatosComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

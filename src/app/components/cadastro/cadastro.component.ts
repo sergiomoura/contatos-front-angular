28bb11b14f481dec82a093df2c873ab5565de12e
Sérgio Moura <smouracalmon@gmail.com>
2021-10-08 01:42:28 -0300

    Alterado o component cadastro para cadastrar usuário

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  public nome:string   = '';
+  public email:string  = '';
+  public senha:string  = '';
+  public conf:string   = '';
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
   }
 
+  cadastrar(){
+    this.authService.cadastrar(this.nome, this.email, this.senha).subscribe(
+      data => console.log(data)
+    )
+  }
 }
